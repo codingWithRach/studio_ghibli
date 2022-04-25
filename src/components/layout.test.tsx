72ffaaa3adc377d52a93e3fa64945579353cc696
Rachel Miles <rachmiles@gmail.com>
2022-04-25 16:29:07 +0100

    Change test to stub the api response

--- src/components/layout.test.tsx
+++ src/components/layout.test.tsx
@@ -1,5 +1,24 @@
 import { render, screen } from "@testing-library/react";
 import Layout from "./layout";
+import { rest } from "msw";
+import { setupServer } from "msw/node";
+
+const dummyData = [
+  {
+    title: "Castle in the Sky",
+  },
+  { title: "Grave of the Fireflies" },
+];
+
+const server = setupServer(
+  rest.get("https://ghibliapi.herokuapp.com/films", (req, res, ctx) => {
+    return res(ctx.json(dummyData));
+  })
+);
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
 
 test("renders the film title Castle in the Sky", async () => {
   render(<Layout />);
