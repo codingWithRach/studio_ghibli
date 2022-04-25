160cdd660c35d4b596f112fe14f9adb82eb4fd86
Rachel Miles <rachmiles@gmail.com>
2022-04-25 16:03:02 +0100

    Remove unneeded line from test

--- src/components/layout.test.tsx
+++ src/components/layout.test.tsx
@@ -3,7 +3,6 @@ import Layout from "./layout";
 
 test("renders the film title Castle in the Sky", async () => {
   render(<Layout />);
-  await screen.findByText("The first film returned is:");
   const titleElement = await screen.findByText(/Castle in the Sky/i);
   expect(titleElement).toBeInTheDocument();
 });
